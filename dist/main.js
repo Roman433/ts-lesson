83df969ed7af65cd7d5fc0234b22551741e93e0b
roman433 <romankachanov0406@gmail.com>
2022-06-21 15:02:53 +0600

    "master"

---
+++ dist/main.js
@@ -0,0 +1,8 @@
+function $53ffd25df6034fb9$var$myPlus(a, b) {
+    return a + b;
+}
+const $53ffd25df6034fb9$var$result = $53ffd25df6034fb9$var$myPlus(1, 2);
+console.log($53ffd25df6034fb9$var$result);
+
+
+//# sourceMappingURL=main.js.map
